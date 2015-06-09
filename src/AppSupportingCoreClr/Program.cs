668747dbd6788e2f20833a90647d130a57ebf592
Kiran Challa <kichalla@microsoft.com>
2015-06-09 10:55:52 -0700

    Initial

---
+++ src/AppSupportingCoreClr/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace AppSupportingCoreClr
+{
+    public static class MyXmlSerializer
+    {
+        public static string GetXml(Type type, Object obj)
+        {
+            var dcs = new DataContractSerializer(type);
+            var ms = new MemoryStream();
+            dcs.WriteObject(ms, obj);
+            ms.Position = 0;
+            var reader = new StreamReader(ms);
+
+            return reader.ReadToEnd();
+        }
+    }
+}
---
+++ src/AppUsingXmlDCS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine(AppSupportingCoreClr.MyXmlSerializer.GetXml(typeof(Person), new Person() { Name = "James" }));
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+        }
+    }
+}
