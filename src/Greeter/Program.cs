24393c170bc471d9df6c6aa1ce090acc5fbe96b3
Kiran Challa <kichalla@microsoft.com>
2015-06-17 11:42:02 -0700

    updated

---
+++ src/Greeter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Greeter
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var names = string.Join(", ", args);
+            if(string.IsNullOrEmpty(names))
+            {
+                Console.WriteLine("Hello, World!");
+            }
+            else
+            {
+                Console.WriteLine("Hello! {0}", names);
+            }
+        }
+    }
+}
